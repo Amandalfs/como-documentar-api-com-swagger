57009e3522a1d3d05da2b5743cd1cf7e5f53a66f
Amandalfs <amandalfs57@gmail.com>
2024-07-10 13:38:39 -0300

    feat: documentando com o swagger Part 1

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,16 @@ import express from "express";
 import cors  from "cors";
 import { routes } from "./routers/users.router";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
+
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(routes)
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export { app };
